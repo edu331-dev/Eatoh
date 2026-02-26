0fd7818469e27cf92972a70970e34c68fdcbf261
Edwin Gitari <e.kabgogitari@gmail.com>
2026-02-26 22:49:46 +0300

    Update on vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [react(), runtimeErrorOverlay()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
